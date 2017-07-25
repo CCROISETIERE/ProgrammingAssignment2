5caed4ec32bbf9f46fb056b7814423aadf2d0f80
ccroisetiere <c.croisetiere@gmail.com>
2017-07-25 16:34:54 -0700

    Added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,15 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        invmatrix <- NULL
-        set <- function(y) {
+        invmatrix <- NULL ## reset solve cache variable when matrix is modified
+        set <- function(y) { ## setter to pass y to x in the parent environment
                 x <<- y
-                invmatrix <<- NULL
+                invmatrix <<- NULL ## reset solve cache variable when matrix is modified
         }
-        get <- function() x  #x is returned 
-        setinverse <- function(inversematrix) invmatrix <<- inversematrix #Setter
-        getinverse <- function() invmatrix #invmatrix is returned
-        list(set = set, get = get, #name each the list elements 
+        get <- function() x  ##  getter will return x 
+        setinverse <- function(inversematrix) invmatrix <<- inversematrix ## setter in parent environment 
+        getinverse <- function() invmatrix ## getter to return solve cache variable
+        list(set = set, get = get, ## name each of the list elements so $ can be use to call function 
              setinverse = setinverse,
              getinverse = getinverse)
         }
@@ -22,12 +22,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        invmatrix <- x$getinverse()
-        if(!is.null(invmatrix)) {
+        invmatrix <- x$getinverse()  ## get value of the solve cache variable 
+        if(!is.null(invmatrix)) { ## if that value if not null then get the value from cash
                 message("getting matrix cached data")
                 return(invmatrix)
         }
-        data <- x$get()
+        data <- x$get() ## if that value is null then calculate it and assign it to the cache variable
         invmatrix <- solve(data, ...)
         x$setinverse(invmatrix)
         invmatrix
